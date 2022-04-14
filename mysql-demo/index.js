eb20a99f5bc249cdf5e9dcbf3b955b9a7f759efd
Aspartame-s <327127960@qq.com>
2022-04-14 23:54:06 +0800

    连接mysql测试

---
+++ mysql-demo/index.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'JqSsb233@',
+    port: '3306',
+    database: 'myblog'
+})
+
+con.connect()
+
+const sql = `select * from users`
+
+con.query(sql, (err, result) => {
+    if(err) {
+        console.error(err)
+        return
+    }
+    console.log(result)
+})
+
+con.end()
